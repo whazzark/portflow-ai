fcf4636d9a43f56c03b588b2e63708910245dee4
Romain Petit <romain.petit@vedana.com>
2026-07-21 22:36:02 +0200

    test: mock window scrolling in jsdom

--- apps/web/src/test/setup.ts
+++ apps/web/src/test/setup.ts
@@ -1,11 +1,16 @@
 import '@testing-library/jest-dom/vitest'
 
 import { cleanup, configure } from '@testing-library/react'
-import { afterAll, afterEach, beforeAll } from 'vitest'
+import { afterAll, afterEach, beforeAll, vi } from 'vitest'
 
 import { server } from './msw/server'
 
 configure({ asyncUtilTimeout: 3_000 })
+
+Object.defineProperty(window, 'scrollTo', {
+  value: vi.fn(),
+  writable: true,
+})
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterEach(() => {
